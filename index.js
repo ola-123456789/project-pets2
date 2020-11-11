dd9005a500bd6e547981801dad6dfede475f2ff5
ola-123456789 <t.lezhava1995@gmail.com>
2020-11-11 21:17:59 +0400

    statements add automatisation
    gancxadebebis damatebis avtomatizacia

--- index.js
+++ index.js
@@ -25,3 +25,29 @@ class Cat extends Pets {
     }
 }
 
+const Statement1 = new Statement ('ჩუქდება თეთრი კნუტი, 1 თვის', 'ლუკა', '599931926', 'თბილისი', 'არის ძალიან საყვარელი',null,0,'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=1200:*');
+
+const Statement2 = new Statement ('ჩუქდება თეთრი კნუტი, 2 თვის', 'ლუკა 2', '599931926', 'თბილისი', 'არის ძალიან საყვარელი',null,0,'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=1200:*');
+
+const Statement3 = new Statement ('ჩუქდება თეთრი კნუტი, 3 თვის', 'ლუკა 3', '599931926', 'თბილისი', 'არის ძალიან საყვარელი',null,0,'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=1200:*');
+
+const statementsArray = [Statement1, Statement2, Statement3];
+
+const newsFeed = document.querySelector('.news-feed');
+
+const appendStatements = (statement) => {
+    for (let i = 0; i < statementsArray.length; i++) {
+        const statementTemplate = `
+        <div class="newsfeed-style">
+        <p>${statement.title}</p>
+        <img src= ${statement.img}>
+        <p>${statement.description}</p>
+        <p class="author-info">${statement.contactPerson}</p>
+        <p class="author-info">${statement.contactPersonNumber}</p>
+    </div>`
+    newsFeed.innerHTML(statementTemplate);
+    }
+}
+
+
+
