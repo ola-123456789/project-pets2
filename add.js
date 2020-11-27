db405816f4231eaf7a0d4973106cd1dbc86385e8
ola-123456789 <t.lezhava1995@gmail.com>
2020-11-28 00:28:04 +0400

    added add functionality with localstorage

--- add.js
+++ add.js
@@ -4,9 +4,23 @@ const imageInput = document.getElementById('image');
 const descriptionInput = document.getElementById('description');
 const contactPersonNameInput = document.getElementById('contactPersonName');
 const contactPersonNumberInput = document.getElementById('contactPersonNumber');
+const imagePreviewDiv = document.getElementById('image-preview');
+const statements = JSON.parse(localStorage.getItem('statements'));
+
+imageInput.addEventListener('change', function() {
+    const imgPreviewInput = `<img src=${imageInput.value} alt="Image Preview" id="imagePreview"></img>`
+    imagePreviewDiv.innerHTML += imgPreviewInput;
+    // const imagePreview = document.getElementById('imagePreview');
+    // imagePreview.src = URL.createObjectURL(this.files[0]);
+    
+})
 
 addBtn.addEventListener('click', () => {
     const addStatement = new Statement (titleInput.value, contactPersonNameInput.value, contactPersonNumberInput.value, null, descriptionInput.value, null, null, imageInput.value);
-    console.log(addStatement);
+    statements.push(addStatement);
+    localStorage.setItem('statements', JSON.stringify(statements));
+    window.location.href = 'index.html';
+    // document.location.href = 'index.html'; ერთი და იგივეა ეს და ზემოთ window.location.href
+    // console.log(addStatement);
 })
 
--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 
 
-const Statement1 = new Statement ('ჩუქდება თეთრი კნუტი, 1 თვის', 'ლუკა', '599931926', 'თბილისი', 'არის ძალიან საყვარელი',null,0,'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=1200:*');
+// const Statement1 = new Statement ('ჩუქდება თეთრი კნუტი, 1 თვის', 'ლუკა', '599931926', 'თბილისი', 'არის ძალიან საყვარელი',null,0,'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=1200:*');
 
-const Statement2 = new Statement ('ჩუქდება თეთრი კნუტი, 2 თვის', 'ლუკა 2', '599931926', 'თბილისი', 'არის ძალიან საყვარელი',null,0,'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=1200:*');
+// const Statement2 = new Statement ('ჩუქდება თეთრი კნუტი, 2 თვის', 'ლუკა 2', '599931926', 'თბილისი', 'არის ძალიან საყვარელი',null,0,'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=1200:*');
 
-const Statement3 = new Statement ('ჩუქდება თეთრი კნუტი, 3 თვის', 'ლუკა 3', '599931926', 'თბილისი', 'არის ძალიან საყვარელი',null,0,'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=1200:*');
+// const Statement3 = new Statement ('ჩუქდება თეთრი კნუტი, 2 თვის', 'ლუკა 3', '599931926', 'თბილისი', 'არის ძალიან საყვარელი',null,0,'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=1200:*');
+// const Statement4 = new Statement ('ჩუქდება რუხი კნუტი, 4 თვის', 'ლუკა 4', '599931926', 'თბილისი', 'არის ძალიან საყვარელი',null,0,'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=1200:*');
+// const Statement5 = new Statement ('ჩუქდება რუხი კნუტი, 5 თვის', 'ლუკა 5', '599931926', 'თბილისი', 'არის ძალიან საყვარელი',null,0,'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=1200:*');
 
-const statementsArray = [Statement1, Statement2, Statement3];
+// const statementsArray = [Statement1, Statement2, Statement3, Statement4, Statement5];
 
 const newsFeed = document.querySelector('.news-feed');
 
@@ -24,9 +26,12 @@ const newsFeed = document.querySelector('.news-feed');
 //     }
 // }
 // appendStatements (statementsArray);
+const statementsAsString = localStorage.getItem('statements');
+const statementsAsObject = JSON.parse(statementsAsString);
+console.log(statementsAsObject);
 
 const appendStatements = (statement) => {
-        const statementTemplate = `
+    const statementTemplate = `
         <div class="newsfeed-style">
         <p>${statement.title}</p>
         <img src= ${statement.img} alt="Dog images" class="image-style">
@@ -37,10 +42,54 @@ const appendStatements = (statement) => {
     newsFeed.innerHTML += statementTemplate;
 }
 
-const appendAllStatements = (statements) => {
-    for (const statement of statements) {
-        appendStatements(statement);
-    }
+// const appendAllStatements = (statements) => {
+//     for (const statement of statements) {
+//         appendStatements(statement);
+//     }
+// }
+// appendAllStatements(statementsAsObject);
+
+for (let i = 0; i < statementsAsObject.length; i++) {
+    appendStatements(statementsAsObject[i]);
 }
-appendAllStatements (statementsArray);
+
+
+// const searchInput = document.getElementById('searchInput');
+// const searchBtn = document.getElementById('searchBtn');
+
+// searchBtn.addEventListener('click', () => {
+//     newsFeed.innerHTML = " ";
+//     if (searchInput.value === '') {
+//         appendAllStatements (statementsArray);
+//         return;
+//     }
+//     const searchOnPage = statementsArray.filter((statement) => {
+//         return statement.title.includes(searchInput.value);
+//     });
+
+//     console.log(searchOnPage);
+//     appendAllStatements (searchOnPage);
+
+// })
+
+//igivea rac: 
+// searchBtn.addEventListener('click', () => {
+//     newsFeed.innerHTML = " ";
+//     const searchOnPage = statementsArray.filter((statement) => {
+//       if (statement.title === searchInput.value) {
+//           return true;
+//       }else {
+//           return false;
+//       }
+//     });
+
+//     console.log(searchOnPage);
+//     appendAllStatements (searchOnPage);
+
+// })
+
+
+
+
+
 
